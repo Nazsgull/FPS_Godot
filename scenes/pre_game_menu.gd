5ed160db16410ff6e23975402f8b7cd2efd862fb
Nazsgull <arevalop10@gmail.com>
2024-01-14 22:59:11 +0100

    Añade menú principal y de multijugador.

---
+++ scenes/pre_game_menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _on_bt_host_pressed():
+	pass # Replace with function body.
+
+func _on_bt_join_pressed():
+	pass # Replace with function body.
+
+func _on_bt_cancel_pressed():
+	pass # Replace with function body.
