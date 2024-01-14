f6a2988f00b3606c9c74062470286edca87a868a
Nazsgull <arevalop10@gmail.com>
2024-01-14 21:13:48 +0100

    Adds a main menu and some decorative shapes to the player

---
+++ scripts/main_menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+func _on_bt_play_pressed():
+	pass # Replace with function body.
+
+
+func _on_bt_options_pressed():
+	pass # Replace with function body.
+
+
+func _on_bt_credits_pressed():
+	pass # Replace with function body.
+
+
+func _on_bt_quit_pressed():
+	pass # Replace with function body.
