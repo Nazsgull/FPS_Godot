be4b11980005104e1b91410a56e5656bab15a725
Nazsgull <arevalop10@gmail.com>
2024-02-13 00:47:34 +0100

    Ya se pueden equipar equipables, con algunos errores.

--- scripts/equippable_pickup.gd
+++ scripts/equippable_pickup.gd
@@ -1,10 +1,19 @@
 extends Node3D
-
+var used = false
 signal pickup(what_item_path:String)
 
 @export var scene:String = "res://scenes/objects/Equippables/test_equippable.tscn"
 
+@onready var timer = $Timer
+@onready var audio_sp = $AudioStreamPlayer
 @onready var player:Player = get_tree().get_nodes_in_group("player")[0]
 
 func _on_area_3d_body_entered(body):
-	player.handle_pickup(scene)
+	if !used:
+		used = true
+		audio_sp.play()
+		player.handle_pickup(scene)
+		timer.start()
+
+func _on_timer_timeout():
+	self.queue_free()
--- scripts/equppables_bar.gd
+++ scripts/equppables_bar.gd
@@ -1,4 +1,5 @@
 extends Node3D
+@onready var pivot = $pivot
 
 var equippables:Array[Equippable] = []
 var currently_equipped:int = 0
@@ -8,15 +9,18 @@ func add_tool(new_scene:String):
 	var new_equippable:Equippable = Equippable.new(new_scene)
 	new_equippable.set_is_working(false)
 	equippables.append(new_equippable)
-	
+	var scene_loaded = load(new_equippable.scene_path)
+	var scene_instance = scene_loaded.instantiate()
+	pivot.add_child(scene_instance)
 
 func _tool_switching_by_input():
 	if Input.is_action_just_pressed("tool_next"):
+		print("next")
 		#We don t have any equippables -> don't switch
 		if equippables.size() == 0:
 			return
 		#Select next equippable
-		if (currently_equipped + 1) > equippables.size():
+		if currently_equipped >= equippables.size():
 			currently_equipped = 0
 			_update_equippable_displayed()
 			return
@@ -25,12 +29,13 @@ func _tool_switching_by_input():
 			_update_equippable_displayed()
 			return
 	if Input.is_action_just_pressed("tool_prev"):
+		print("prev")
 		#We don t have any equippables -> don't switch
 		if equippables.size() == 0:
 			return
 		#Select previous equippable
-		if (currently_equipped - 1) < 0:
-			currently_equipped = equippables.size()-1
+		if (currently_equipped - 1) > 0:
+			currently_equipped -= 1
 			_update_equippable_displayed()
 			return
 
@@ -38,7 +43,7 @@ func _update_equippable_displayed():
 	for each_equippable in equippables:
 		each_equippable.set_is_working(false)
 	if equippables.size()>0:
-		equippables[currently_equipped].set_is_working(true)
+		equippables[currently_equipped-1].set_is_working(true)
 	
 func _process(delta):
 	_tool_switching_by_input()
