23b87bfc5c216d093602e76c3ba9ab7104b7bf94
Nazsgull <arevalop10@gmail.com>
2024-04-25 23:57:22 +0200

    Añade una confirmación para volver al menú principal
    Añade un pop up que pregunta si volver al menú principal o no.

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -1,5 +1,5 @@
 extends Control
-
+@onready var conf_diag = $ConfirmationDialog
 @onready var player_root = $"../../../" #we jump 3 up in the player component hierarchy
 @onready var main_menu_path = "res://scenes/UI/main_menu.tscn"
 func handle_resume_pressed() -> void:
@@ -9,5 +9,11 @@ func handle_options_pressed() -> void:
 	pass
 
 func handle_quit_pressed() -> void:
+	conf_diag.show()
+
+func handle_cancel_pressed() -> void:
+	conf_diag.hide()
+
+func handle_quit_confirmed() -> void:
 	player_root.handle_pause()
 	get_tree().change_scene_to_file(main_menu_path)
